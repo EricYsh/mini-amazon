8e0538a3a28384b5dbc75ff4eb0e1bc83a67edf0
Shanghui Yin <sy275@duke.edu>
2024-02-17 02:34:49 -0500

    add wishlist_add function

--- app/wishlist.py
+++ app/wishlist.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask_login import current_user
 from flask import jsonify
+from flask import redirect, url_for
 import datetime
 
 
@@ -22,3 +23,13 @@ def wishlist():
     else:
         return jsonfiy({}), 404
     return jsonify([item.__dict__ for item in items])
+
+@bp.route('/wishlist/add/<int:product_id>', methods=['POST'])
+def wishlist_add(product_id):
+    # add a product to the current user's wishlist
+    if current_user.is_authenticated:
+        WishlistItem.add_wishlist_item(current_user.id, product_id, datetime.datetime.now())
+    else:
+        # TODO redirect it to an error page
+        return None
+    return redirect(url_for('/wishlist.wishlist'))
