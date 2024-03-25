62b93e8e8345937b0049f877e3e10eaa12b5e68d
Yilin Yang <yy379@duke.edu>
2024-03-25 10:28:09 -0400

    update front end

--- app/usercomment.py
+++ app/usercomment.py
@@ -2,13 +2,19 @@
 # Given a user id, find the 5 most recent feedback they posted
 from flask import Blueprint, jsonify, request
 from .models.productcomment import ProductComment
+from .models.sellercomment import SellerComment
+from flask_login import current_user
+from flask import render_template
 
-bp = Blueprint('usercomment', __name__, url_prefix='/usercomments')
+bp = Blueprint('usercomment', __name__)
 
-@bp.route('/<int:userid>', methods=['GET'])
-def get_user_comments(userid):
+@bp.route('/usercomments', methods=['GET'])
+def usercomments():
     # Use ProductComment get_comments_by_user(userid)
-    comments = ProductComment.get_comments_by_user(userid)
+    product_comments = ProductComment.get_product_comments_by_user(current_user.id)
+    seller_comments = SellerComment.get_seller_comments_by_user(current_user.id)
     # return the comments in JSON format
-    return jsonify(comments)
+    return render_template('usercomment.html',
+                       product_comments=product_comments,
+                       seller_comments=seller_comments)
 
