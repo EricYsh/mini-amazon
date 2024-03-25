18ce74fe264be7012d25425e28c59d3c50a5493a
Yilin Yang <yy379@duke.edu>
2024-03-25 09:50:43 -0400

    update

---
+++ drop_all_table.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS 
+    categories, 
+    products, 
+    orderitems, 
+    sellerinventories, 
+    pricehistory, 
+    carts, 
+    orders, 
+    coupons, 
+    productcomments, 
+    sellercomments, 
+    users
+CASCADE;
