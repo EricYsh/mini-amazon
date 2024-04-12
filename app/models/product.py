7c1646614b133de32bc2ee7ecde707da779d29cc
raplis <1195624612@qq.com>
2024-04-12 18:32:52 -0400

    product_items and product

--- app/models/product.py
+++ app/models/product.py
@@ -11,46 +11,179 @@ from flask import current_app as app
 # );
 
 class Product:
-    def __init__(self, id, categoryid, name, description, image, price=None):
+    def __init__(self, id, categoryid, name, description, image, price = None, rating = None):
         self.id = id
         self.categoryid = categoryid
         self.name = name
         self.description = description
         self.image = image
         self.price = price
+        self.rating = rating
 
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT id, name, price, available
-FROM Products
-WHERE id = :id
-''',
+            SELECT id, name, price, available
+            FROM Products
+            WHERE id = :id
+            ''',
                               id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
     def get_all():
         rows = app.db.execute('''
-SELECT *
-FROM Products
-''',)
+            SELECT *
+            FROM Products
+            ''',)
         return [Product(*row) for row in rows]
 
     @staticmethod
     def get_top_k_expensive_products(k):
         rows = app.db.execute('''
-    SELECT p.id, p.categoryid, p.name, p.description, p.image, ph.price
-    FROM Products p
-    JOIN SellerInventories si ON p.id = si.productid
-    JOIN PriceHistory ph ON si.id = ph.inventoryid
-    JOIN (
-        SELECT inventoryid, MAX(time_changed) AS latest_time
-        FROM PriceHistory
-        GROUP BY inventoryid
-    ) ph_latest ON ph.inventoryid = ph_latest.inventoryid AND ph.time_changed = ph_latest.latest_time
-    ORDER BY ph.price DESC
-    LIMIT :k
-    ''',
+            SELECT p.id, p.categoryid, p.name, p.description, p.image, ph.price, COALESCE(ROUND(avg_rating.avg_rate, 2), 0)
+            FROM Products p
+            JOIN SellerInventories si ON p.id = si.productid
+            JOIN PriceHistory ph ON si.id = ph.inventoryid
+            JOIN (
+                SELECT inventoryid, MAX(time_changed) AS latest_time
+                FROM PriceHistory
+                GROUP BY inventoryid
+            ) ph_latest ON ph.inventoryid = ph_latest.inventoryid AND ph.time_changed = ph_latest.latest_time
+            LEFT JOIN (
+                SELECT productid, AVG(rate) AS avg_rate
+                FROM ProductComments
+                GROUP BY productid
+            ) avg_rating ON p.id = avg_rating.productid
+            ORDER BY ph.price DESC
+            LIMIT :k
+            ''',
                             k=k)
         return [Product(*row) for row in rows]
+    
+
+
+    @staticmethod
+    def search_products_by_name(search, category_id=None):
+        query_params = {'search': search}
+        category_condition = ''
+
+        if category_id:
+            category_condition = 'AND p.categoryid = :category_id'
+            query_params['category_id'] = category_id
+
+        rows = app.db.execute(f'''
+            SELECT p.id, p.categoryid, p.name, p.description, p.image, ph.price, COALESCE(ROUND(avg_rating.avg_rate, 2), 0) AS average_rating
+            FROM Products p
+            JOIN SellerInventories si ON p.id = si.productid
+            JOIN PriceHistory ph ON si.id = ph.inventoryid
+            JOIN (
+                SELECT inventoryid, MAX(time_changed) AS latest_time
+                FROM PriceHistory
+                GROUP BY inventoryid
+            ) ph_latest ON ph.inventoryid = ph_latest.inventoryid AND ph.time_changed = ph_latest.latest_time
+            LEFT JOIN (
+                SELECT productid, AVG(rate) AS avg_rate
+                FROM ProductComments
+                GROUP BY productid
+            ) avg_rating ON p.id = avg_rating.productid
+            WHERE p.name ILIKE '%' || :search || '%' {category_condition}
+            ORDER BY ph.price DESC
+            LIMIT 50
+            ''', **query_params)
+        return [Product(*row) for row in rows]
+
+    
+    @staticmethod
+    def search_products_by_description(search, category_id=None):
+        query_params = {'search': search}
+        category_condition = ''
+
+        # 如果提供了 category_id，则在查询中加入一个额外的条件
+        if category_id:
+            category_condition = 'AND p.categoryid = :category_id'
+            query_params['category_id'] = category_id
+
+        rows = app.db.execute(f'''
+            SELECT p.id, p.categoryid, p.name, p.description, p.image, ph.price, COALESCE(ROUND(avg_rating.avg_rate, 2), 0) AS average_rating
+            FROM Products p
+            JOIN SellerInventories si ON p.id = si.productid
+            JOIN PriceHistory ph ON si.id = ph.inventoryid
+            JOIN (
+                SELECT inventoryid, MAX(time_changed) AS latest_time
+                FROM PriceHistory
+                GROUP BY inventoryid
+            ) ph_latest ON ph.inventoryid = ph_latest.inventoryid AND ph.time_changed = ph_latest.latest_time
+            LEFT JOIN (
+                SELECT productid, AVG(rate) AS avg_rate
+                FROM ProductComments
+                GROUP BY productid
+            ) avg_rating ON p.id = avg_rating.productid
+            WHERE p.description ILIKE '%' || :search || '%' {category_condition}
+            ORDER BY ph.price DESC
+            LIMIT 50
+            ''', **query_params)
+        return [Product(*row) for row in rows]
+
+
+
+    @staticmethod
+    def get_product_by_id(product_id):
+        rows = app.db.execute('''
+            SELECT p.id, p.categoryid, p.name, p.description, p.image, ph.price, COALESCE(ROUND(avg_rating.avg_rate, 2), 0) AS average_rating
+            FROM Products p
+            JOIN SellerInventories si ON p.id = si.productid
+            JOIN PriceHistory ph ON si.id = ph.inventoryid
+            JOIN (
+                SELECT inventoryid, MAX(time_changed) AS latest_time
+                FROM PriceHistory
+                GROUP BY inventoryid
+            ) ph_latest ON ph.inventoryid = ph_latest.inventoryid AND ph.time_changed = ph_latest.latest_time
+            LEFT JOIN (
+                SELECT productid, AVG(rate) AS avg_rate
+                FROM ProductComments
+                GROUP BY productid
+            ) avg_rating ON p.id = avg_rating.productid
+            WHERE p.id = :product_id
+            ''',
+                            product_id=product_id)
+        if rows:
+            row = rows[0]
+            return Product(*row)
+        else:
+            return None
+
+
+    @staticmethod
+    def get_reviews(product_id):
+        reviews = app.db.execute('''
+            SELECT pc.id, pc.comment, pc.time_commented, pc.rate, u.firstname, u.lastname
+            FROM ProductComments pc
+            JOIN Users u ON pc.userid = u.id
+            WHERE pc.productid = :product_id
+            ORDER BY pc.time_commented DESC
+            ''',
+                                product_id=product_id)
+
+        return reviews if reviews is not None else None
+
+
+    @staticmethod
+    def get_sellers(product_id):
+        sellers = app.db.execute('''
+            SELECT u.id, u.firstname, u.lastname, u.email, si.quantity
+            FROM Users u
+            JOIN SellerInventories si ON u.id = si.sellerid
+            WHERE si.productid = :product_id
+            ''',
+            product_id=product_id)
+        return sellers if sellers is not None else None
+        
+    @staticmethod
+    def get_all_categories():
+        rows = app.db.execute('''
+            SELECT id, name
+            FROM Categories
+            ORDER BY name ASC
+        ''')
+        return rows  
--- app/product.py
+++ app/product.py
@@ -1,5 +1,3 @@
-# This class is no longer used in the following milestones.
-
 from flask import render_template, redirect, url_for, request
 
 from .models.product import Product
@@ -7,13 +5,48 @@ from .models.product import Product
 from flask import Blueprint
 bp = Blueprint('product', __name__)
 
-
 @bp.route('/product', methods=['GET', 'POST'])
 def product():
-    if request.method == 'POST':
-        k = request.form.get('k')
-        if k.isdigit():  # 确保k是数字
-            k = int(k)  # 将k转换为整数
-            product_items = Product.get_top_k_expensive_products(k)  # 使用k作为参数
-            return render_template('products.html', products=product_items)  # 传递product_items变量        return render_template('products.html', products=products)
-    return render_template('find_products.html')
+    if request.method == 'GET':
+        # k = request.form.get('k')
+        # if k.isdigit():  # Ensure k is a number
+        #     k = int(k)  # Convert k to an integer
+        product_items = Product.get_top_k_expensive_products(20)  # Use k as the parameter
+        categories = Product.get_all_categories()  
+        return render_template('products.html', products=product_items, categories=categories)  # Pass the product_items variable
+    return render_template('404.html')
+
+@bp.route('/product/search', methods=['GET'])
+def product_search():
+    search = request.args.get('keyword', '')
+    search_type = request.args.get('search_type', 'name')
+    category_id = request.args.get('category', None)  # 获取类别ID
+    sort = request.args.get('sort', None)
+
+    # 根据搜索类型和类别筛选产品
+    if search_type == 'name':
+        products = Product.search_products_by_name(search, category_id)
+    elif search_type == 'description':
+        products = Product.search_products_by_description(search, category_id)
+    else:
+        return render_template('404.html')
+
+    # 排序逻辑保持不变
+    if sort:
+        reverse = True if 'desc' in sort else False
+        key = 'price' if 'price' in sort else 'rating'
+        products = sorted(products, key=lambda x: getattr(x, key), reverse=reverse)
+
+    categories = Product.get_all_categories()  
+    return render_template('products.html', products=products, categories=categories, search_type=search_type, sort=sort, selected_category=category_id)
+
+
+
+@bp.route('/products/<int:product_id>', methods=['GET'])
+def product_detail(product_id):
+    product = Product.get_product_by_id(product_id)
+    sellers = Product.get_sellers(product_id)
+    reviews = Product.get_reviews(product_id)
+    return render_template('product_detail.html', product=product, sellers = sellers, reviews = reviews) 
+
+
