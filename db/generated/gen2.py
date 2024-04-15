219753f80da9abe6a2f9aca4b7de829c94e6f754
Qingqian Wang <qw116@duke.edu>
2024-04-15 17:50:27 -0400

    db bug fix

--- db/generated/gen2.py
+++ db/generated/gen2.py
@@ -58,9 +58,9 @@ def gen_products(num_products):
 
 
 def gen_sellerinventories(num_sellerinvertories):
-    with open('Sellerinventories.csv', 'w') as f:
+    with open('SellerInventories.csv', 'w') as f:
         writer = get_csv_writer(f)
-        print('Sellerinventories...', end=' ', flush=True)
+        print('SellerInventories...', end=' ', flush=True)
         for sid in range(num_sellerinvertories):
             if sid % 10 == 0:
                 print(f'{sid}', end=' ', flush=True)
@@ -72,9 +72,9 @@ def gen_sellerinventories(num_sellerinvertories):
     return
 
 def gen_orderitems(num_orderitems):
-    with open('Orderitems.csv', 'w') as f:
+    with open('OrderItems.csv', 'w') as f:
         writer = get_csv_writer(f)
-        print('Orderitems...', end=' ', flush=True)
+        print('OrderItems...', end=' ', flush=True)
         for oiid in range(num_orderitems):
             if oiid % 10 == 0:
                 print(f'{oiid}', end=' ', flush=True)
