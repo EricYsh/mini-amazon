aeb3beb5d99e4a264ec4930cd5964229070d84b0
Yilin Yang <yy379@duke.edu>
2024-04-16 22:02:12 -0400

    update edit comment

--- app/usercomment.py
+++ app/usercomment.py
@@ -5,8 +5,16 @@ from .models.productcomment import ProductComment
 from .models.sellercomment import SellerComment
 from flask_login import current_user
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
 
 bp = Blueprint('usercomment', __name__)
+
+class EditCommentForm(FlaskForm):
+    comment = StringField('Comment', validators=[DataRequired()])
+    rate = IntegerField('Rate', validators=[DataRequired()])
+    submit = SubmitField('Update Comment')
 
 @bp.route('/usercomments', methods=['GET'])
 def usercomments():
@@ -18,3 +26,31 @@ def usercomments():
                        product_comments=product_comments,
                        seller_comments=seller_comments)
 
+# test
+@bp.route('/edit_product_comment/<int:comment_id>', methods=['GET', 'POST'])
+def edit_product_comment(comment_id):
+    comment = ProductComment.query.get_or_404(comment_id)
+    if comment.userid != current_user.id:
+        abort(403)  # Ensure that the current user owns the comment
+    form = EditCommentForm(obj=comment)
+    if form.validate_on_submit():
+        comment.comment = form.comment.data
+        comment.rate = form.rate.data
+        db.session.commit()
+        flash('Your comment has been updated.')
+        return redirect(url_for('usercomment.usercomments'))
+    return render_template('edit_comment.html', form=form, title='Edit Product Comment')
+
+@bp.route('/edit_seller_comment/<int:comment_id>', methods=['GET', 'POST'])
+def edit_seller_comment(comment_id):
+    comment = SellerComment.query.get_or_404(comment_id)
+    if comment.userid != current_user.id:
+        abort(403)  # Ensure that the current user owns the comment
+    form = EditCommentForm(obj=comment)
+    if form.validate_on_submit():
+        comment.comment = form.comment.data
+        comment.rate = form.rate.data
+        db.session.commit()
+        flash('Your comment has been updated.')
+        return redirect(url_for('usercomment.usercomments'))
+    return render_template('edit_comment.html', form=form, title='Edit Seller Comment')
